a075c432a995c4d5f38e9252f38704924f1775e5
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-05-20 20:38:47 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/tuananh/nhansinh_59132120/trannhansinh_59132120_recycleview/Controller/CartController.java
@@ -0,0 +1,30 @@
+package vn.edu.ntu.tuananh.nhansinh_59132120.trannhansinh_59132120_recycleview.Controller;
+
+import android.app.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.ntu.tuananh.nhansinh_59132120.trannhansinh_59132120_recycleview.Model.Product;
+
+public class CartController extends Application implements ICartController {
+    List<Product> listProducts = new ArrayList<>();
+
+    public CartController(){
+        listProducts.add(new Product("Khoai Lang", 25000, "Đây là khoai lang"));
+        listProducts.add(new Product("Khoai Tây", 35000, "Đây là khoai Tây"));
+        listProducts.add(new Product("Khoai Mì", 15000, "Đây là khoai Mì"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+        listProducts.add(new Product("Khoai Tím", 20000, "Đây là khoai Tím"));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return listProducts;
+    }
+}
+
